f626db5857ed67c539da6384bf30f7faa4274fc2
Victoria Fathi <victoria.fathi@gmail.com>
2023-07-21 10:25:14 +0200

    fix ROOT_DIR export

--- config/install.sh
+++ config/install.sh
@@ -3,12 +3,14 @@
 # Prompt for environment name
 read -p "Enter the environment name: " env_name
 
-SCRIPTPATH="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"  #get directory path
-export ROOT_DIR=$(builtin cd $SCRIPTPATH/..; pwd) #Create environment variable for root direction
-
 # Install required Python libraries and tools in the specified conda environment
-conda create -n "$env_name" python pandas scipy seaborn plotly jupyter -y
+conda create -n "$env_name" python pandas scipy seaborn plotly jupyter dotenv -y
 conda activate "$env_name"
-pip install ipython
+#pip install ipython
+
+# VAR ENV 
+SCRIPTPATH="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )" #get directory path
+conda env config vars set ROOT_DIR=$(cd "$SCRIPTPATH"/.. && pwd)
+conda deactivate "$env_name"
 
 echo "Installation completed successfully."
